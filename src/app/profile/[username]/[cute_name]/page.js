983986f25f18b3a7b83d531285e81bfc62ffb532
Enrico <enricovdarend@gmail.com>
2024-07-20 12:44:26 +0200

    /username now redirects to active profile slot

--- src/app/profile/[username]/[cute_name]/page.js
+++ src/app/profile/[username]/[cute_name]/page.js
@@ -1,12 +1,15 @@
 require("dotenv").config()
 
-import { redirect } from "next/navigation"
 import { getData } from "@/app/Util/functions"
+import { getNetworth } from "skyhelper-networth"
 
 export default async function Page({ params }) {
     const data = await getData(`https://api.mojang.com/users/profiles/minecraft/${params.username}`)
     const profiles = await getData(`https://api.hypixel.net/v2/skyblock/profiles?key=${process.env.HYPIXEL_API_KEY}&uuid=${data.id}`)
     const selectedProfile = profiles.profiles.filter((profile) => profile.selected)[0]
-    console.log(selectedProfile)
-    redirect(`/profile/${params.username}/${selectedProfile.cute_name}`)
+    const museumData = await getData(`https://api.hypixel.net/v2/skyblock/museum?key=${process.env.HYPIXEL_API_KEY}&profile=${selectedProfile.profile_id}`)
+    console.log(museumData)
+    return (
+        <div>My Post: {params.username} {params.cute_name} <br></br> {selectedProfile.cute_name} </div>
+    )
 }
