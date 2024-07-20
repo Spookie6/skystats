067dec6ead2dc7b2d88eee5fe0288aa674c31d7b
Samuel Horner <samkoseyhorner@gmail.com>
2024-07-20 16:17:46 +0100

    Added API parsing

--- src/app/profile/[username]/page.js
+++ src/app/profile/[username]/page.js
@@ -7,6 +7,6 @@ export default async function Page({ params }) {
     const data = await getData(`https://api.mojang.com/users/profiles/minecraft/${params.username}`)
     const profiles = await getData(`https://api.hypixel.net/v2/skyblock/profiles?key=${process.env.HYPIXEL_API_KEY}&uuid=${data.id}`)
     const selectedProfile = profiles.profiles.filter((profile) => profile.selected)[0]
-    console.log(selectedProfile)
+    // console.log(selectedProfile)
     redirect(`/profile/${params.username}/${selectedProfile.cute_name}`)
 }
